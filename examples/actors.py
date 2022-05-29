f06012d72d244c3c68a4b266583ac63c3e7c130e
cybercti <cybercti@localhost>
2022-05-29 14:01:29 +0800

    Added pretty print output

--- examples/actors.py
+++ examples/actors.py
@@ -15,4 +15,4 @@ password = environ['MAV4_PASS']
 if __name__ == "__main__":
     client = MAV4(username, password)
     items = client.get_items("actor", limit=5000)
-    print(dumps(items))
+    print(dumps(items, indent=4))
