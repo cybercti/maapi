77ef06e5f0ef861535266c6519e765bbdc250093
cybercti <cybercti@localhost>
2022-06-11 23:53:35 +0800

    More printing pretty

--- src/maapi/utils/cli/dtm_cli.py
+++ src/maapi/utils/cli/dtm_cli.py
@@ -4,50 +4,66 @@ Mandiant Advantage Threat Intelligence CLI
 """
 
 # Native Imports
-from os import environ, path
+from os import environ
 import logging
-from datetime import datetime
 from json import dumps
+from xmlrpc.client import boolean
 
 # 3rd-Party Imports
 import click
 from maapi.dtm import DTM
-
-logging.basicConfig(filename=None, encoding='utf-8', level=logging.WARNING)
 
 logger = logging.getLogger(__name__)
 username = environ['MAV4_USER']
 password = environ['MAV4_PASS']
 
 @click.group()
-def dtm():
+@click.option('--debug/--no-debug', default=False)
+def dtm(debug):
     """
     DTM CLI of MAAPI
     """
+    if debug:
+        logging.basicConfig(filename=None, encoding='utf-8', level=logging.DEBUG)
+        logger.debug('Debug is on')
+    else:
+        logging.basicConfig(filename=None, encoding='utf-8', level=logging.WARNING)
+
+def print_monitor_list(client: DTM, limit: int) -> None:
+    """
+    Get and print a list of monitors
+    """
+    items = client.get_monitor_list(limit)
+    logging.debug(dumps(items, indent=4))
+    print('cafvdt4d4k6sqiegfml0 ------  Credential Monitoring')
+    print('┌───────────────────────────────────────────────────────────────┐')
+    print('│ Monitor ID          |  Status  |  Name                        │')
+    print('└───────────────────────────────────────────────────────────────┘')
+    for item in items['monitors']:
+        statuses = ""
+        if item['enabled']:
+            statuses += "\U00002705"
+        else:
+            statuses += "\U0000274C"
+        if item['email_notify_enabled']:
+            statuses += "\U0001F4EC"
+        else:
+            statuses += "--"
+        if item['email_notify_immediate']:
+            statuses += "\U0001F3C1"
+        else:
+            statuses += "--"
+        print(f"  {item['id']}   {statuses}     {item['name']}")
+
 
 @dtm.command('monitor')
 @click.argument('command', type=click.Choice(['list', 'activate', 'deactivate', 'delete']))
-def monitor(command):
+@click.option('--limit', default=50, help="Number of items to retrieve")
+def monitor(command, limit):
     """
     Monitor related functionality
     """
 
     client = DTM(username, password)
     if command == 'list':
-        items = client.get_monitor_list()
-        logging.debug(dumps(items, indent=4))
-        for item in items['monitors']:
-            statuses = ""
-            if item['enabled']:
-                statuses += "\U00002705"
-            else:
-                statuses += "\U0000274C"
-            if item['email_notify_enabled']:
-                statuses += "\U0001F4EC"
-            else:
-                statuses += "--"
-            if item['email_notify_immediate']:
-                statuses += "\U0001F3C1"
-            else:
-                statuses += "--"
-            print(f"{item['id']} {statuses}  {item['name']}")
+        print_monitor_list(client, limit)
