365bc71d1252dad35b7f54038572bca1f81b8d8b
cybercti <cybercti@localhost>
2022-06-12 12:33:42 +0800

    Moved MAV4 functionality to parent class.

--- src/maapi/mati.py
+++ src/maapi/mati.py
@@ -1,84 +1,22 @@
 # Native Import
 from ipaddress import ip_address
-from time import time
 import logging
-from os import path
-from json import dumps
 
-# Third party imports
-from requests import Session
-from requests.auth import HTTPBasicAuth
+# Local Imports
+from maapi import MAAPI
 
 logger = logging.getLogger(__name__)
 
-class MAV4:
+class MAV4(MAAPI):
     """
-    API client for Mandiant Advantage API v4
+    API client for Mandiant Advantage API v4 (MATI)
     """
 
     APP_NAME = "cybercti client"
 
-    def __init__(self, username=None, password=None, token=None, host="https://api.intelligence.fireeye.com"):
-        self.username = username
-        self.password = password
-        self.host = host
-        self._session = Session()
-        self._request_headers = {
-            "accept": "application/json",
-            "X-App-Name": MAV4.APP_NAME,
-        }
-        if token is None:
-            self._auth()
-        else:
-            self.token = token["token"]
-            self._update_auth_struct()
-            self.token_expiration_time = token["token_expiration_time"] # But override the expiration.
-
-    def _update_auth_struct(self):
-        self.token_expiration_time = time() + self.token["expires_in"]  # Stored as seconds since epoch as a float.
-        self.bearer_token = f"Bearer {self.token['access_token']}"
-        self._request_headers["Authorization"] = self.bearer_token
-
-    def _auth(self):
-        """
-        Returns an access token.
-        """
-        auth = HTTPBasicAuth(self.username, self.password)
-        url = f"{self.host}/token"
-        data = { "grant_type": "client_credentials" }
-        headers = { "content-type": "application/x-www-form-urlencoded" }
-        self.token = self._session.post(url=url, data=data, headers=headers, auth=auth).json()
-        self._update_auth_struct()
-        logger.debug(self.token)
-
-
-    def _save_auth(self):
-        token_file = path.expanduser("~/.mav4_token")
-        with open(token_file, "w", encoding="utf-8") as outfile:
-            outfile.write(dumps({"token": self.token, "token_expiration_time": self.token_expiration_time}, indent = 4))
-
-    def _http_request(self, func, url, params=None, ** kwargs):
-        """
-        Validate the Token is still valid, renewing as needed before
-        passing on the request to the Python Requests Session.
-        """
-        # call the endpoint
-        token_time_left = self.token_expiration_time - time()
-        if token_time_left > 60: # 60 second buffer
-            logger.debug("There is still %.2f seconds left in the token", token_time_left)
-        else:
-            logger.debug("Token expired, renewing Token.")
-            self._auth()
-            token_time_left = self.token_expiration_time - time()
-            logger.debug("Renewed. There is now %.2f seconds left in the token", token_time_left)
-        response = func(url, headers=self._request_headers, params=params, ** kwargs)
-        return response
-
-    def _http_get(self, * args, ** kwargs):
-        return self._http_request(self._session.get, * args, ** kwargs)
-
-    def _http_post(self, * args, ** kwargs):
-        return self._http_request(self._session.post, * args, ** kwargs)
+    def __init__(self, *args, **kwargs):
+        self.sub_type = 'MATI'
+        super().__init__(*args, **kwargs)
 
     def _retrieve(self, item_type, start=None, end=None, limit=25, value=None, next_pointer=None):
         url = f"{self.host}/v4/{item_type}"
