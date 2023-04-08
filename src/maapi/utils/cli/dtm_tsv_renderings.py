7897159467beb687d17ae80d551b70f2b773a501
cybercti <cybercti@localhost>
2023-04-08 21:34:14 +0800

    Added several new columns.

--- src/maapi/utils/cli/dtm_tsv_renderings.py
+++ src/maapi/utils/cli/dtm_tsv_renderings.py
@@ -9,6 +9,7 @@ def render_tsv_entry_shop_listing_cc_header():
         'Name Available\tDOB Available\tSSN Available\tPhone Available\tOwner Name\tOwner Phone\tOwner Street Address\t'
         'Owner City\tOwner Region\tOwner Postal Code\tOwner Country'
     )
+
 def render_tsv_entry_shop_listing_cc(document):
     return (
         f'{document["timestamp"]}\t'
@@ -16,20 +17,24 @@ def render_tsv_entry_shop_listing_cc(document):
         f'{document["payment_card"].get("type","")}\t'
         f'{document["payment_card"].get("brand","")}\t'
         f'{document["payment_card"].get("issuer","")}\t'
-        f'{document["shop"]["name"]}\t{document["price"]}\t{document["currency"]}\t{document["item_qty"]}\t'
-        f'{document["payment_card"]["expiry_date"]}\t{document["payment_card"].get("service_code","")}\t'
-        f'{document["batch"]["name"]}\t'
+        f'{document["shop"].get("name")}\t'
+        f'{document["price"]}\t'
+        f'{document["currency"]}\t'
+        f'{document["item_qty"]}\t'
+        f'{document["payment_card"].get("expiry_date","")}\t'
+        f'{document["payment_card"].get("service_code","")}\t'
+        f'{document["batch"].get("name")}\t'
         f'{document.get("data_availability",{}).get("track1_available","")}\t'
-        f'{document.get("data_availability",{}).get("track2_available","")}'
-        f'{document.get("data_availability",{}).get("name_available","")}'
-        f'{document.get("data_availability",{}).get("dob_available","")}'
-        f'{document.get("data_availability",{}).get("ssn_available","")}'
-        f'{document.get("data_availability",{}).get("phone_available","")}'
-        f'{document["payment_card"].get("owner",{}).get("identity",{}).get("name","")}' # Name
-        f'{document["payment_card"].get("owner",{}).get("contact",{}).get("phone","")}' # Phone
-        f'{document["payment_card"].get("owner",{}).get("contact",{}).get("geo_location",{}).get("street_address","")}' # Address
-        f'{document["payment_card"].get("owner",{}).get("contact",{}).get("geo_location",{}).get("city","")}' # City
-        f'{document["payment_card"].get("owner",{}).get("contact",{}).get("geo_location",{}).get("region","")}' # Region
-        f'{document["payment_card"].get("owner",{}).get("contact",{}).get("geo_location",{}).get("postal_code","")}' # Postal Code
-        f'{document["payment_card"].get("owner",{}).get("contact",{}).get("geo_location",{}).get("country_code","")}' # Country Code
+        f'{document.get("data_availability",{}).get("track2_available","")}\t'
+        f'{document.get("data_availability",{}).get("name_available","")}\t'
+        f'{document.get("data_availability",{}).get("dob_available","")}\t'
+        f'{document.get("data_availability",{}).get("ssn_available","")}\t'
+        f'{document.get("data_availability",{}).get("phone_available","")}\t'
+        f'{document["payment_card"].get("owner",{}).get("identity",{}).get("name","")}\t'
+        f'{document["payment_card"].get("owner",{}).get("contact",{}).get("phone","")}\t'
+        f'{document["payment_card"].get("owner",{}).get("contact",{}).get("geo_location",{}).get("street_address","")}\t'
+        f'{document["payment_card"].get("owner",{}).get("contact",{}).get("geo_location",{}).get("city","")}\t'
+        f'{document["payment_card"].get("owner",{}).get("contact",{}).get("geo_location",{}).get("region","")}\t'
+        f'{document["payment_card"].get("owner",{}).get("contact",{}).get("geo_location",{}).get("postal_code","")}\t'
+        f'{document["payment_card"].get("owner",{}).get("contact",{}).get("geo_location",{}).get("country_code","")}'
     )
