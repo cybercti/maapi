d724065fc28e631a83b312eedd0617709763c7c8
cybercti <cybercti@localhost>
2023-04-08 21:50:35 +0800

    Added several new columns.

--- src/maapi/utils/cli/dtm_tsv_renderings.py
+++ src/maapi/utils/cli/dtm_tsv_renderings.py
@@ -8,7 +8,7 @@ def render_tsv_entry_shop_listing_cc_header():
     Return a header row for CC shop listing.
     """
     return (
-        'timestamp\tBIN\tType\tBrand\tIssuer\tShop\tPrice\tCurrency\tQuantity\tExpiry Date\tService Code\tBatch\tTrack1 Available\tTrack2 Available\t'
+        'timestamp\tShop\tBIN\tType\tBrand\tIssuer\tProduct\tPrice\tCurrency\tQuantity\tExpiry Date\tService Code\tBatch\tTrack1 Available\tTrack2 Available\t'
         'Name Available\tDOB Available\tSSN Available\tPhone Available\tOwner Name\tOwner Phone\tOwner Street Address\t'
         'Owner City\tOwner Region\tOwner Postal Code\tOwner Country Code'
     )
@@ -19,11 +19,12 @@ def render_tsv_entry_shop_listing_cc(document):
     """
     return (
         f'{document["timestamp"]}\t'
+        f'{document["shop"].get("name")}\t'
         f'{document["payment_card"].get("partial_number_prefix","")}\t'
         f'{document["payment_card"].get("type","")}\t'
         f'{document["payment_card"].get("brand","")}\t'
         f'{document["payment_card"].get("issuer","")}\t'
-        f'{document["shop"].get("name")}\t'
+        f'{document["payment_card"].get("product","")}\t'
         f'{document["price"]}\t'
         f'{document["currency"]}\t'
         f'{document["item_qty"]}\t'
